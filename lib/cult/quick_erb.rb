3dfcf3d61939d07c68f6aa9b19a74f4f2432f8ac
Mike A. Owens <mike@filespanker.com>
2016-08-06 05:15:05 -0400

    QuickErb string refinements for escaping

--- lib/cult/quick_erb.rb
+++ lib/cult/quick_erb.rb
@@ -1,17 +1,55 @@
 require 'erb'
-require 'shellwords'
 
 module Cult
   class QuickErb
+
+    # Alright!  We found a use for refinements!
+    module Refinements
+      module Util
+        module_function
+        def squote(s)
+          "'" + s.gsub("'", "\\\\\'") + "'"
+        end
+      end
+
+      refine String do
+        alias_method :quote, :to_json
+        alias_method :q, :quote
+
+        def squote
+          Util.squote(self)
+        end
+        alias_method :sq, :squote
+
+        def slash
+          Shellwords.escape(self)
+        end
+      end
+
+      refine Array do
+        def quote(sep = ' ')
+          map(&:to_json).join(sep)
+        end
+        alias_method :q, :quote
+
+        def squote(sep = ' ')
+          map {|v| Util.squote(v) }.join(sep)
+        end
+        alias_method :sq, :squote
+
+        def slash
+          Shellwords.join(self)
+        end
+      end
+    end
+
     class Context
+      using Refinements
+
       def initialize(**kw)
         kw.each do |k,v|
           define_singleton_method(k) { v }
         end
-      end
-
-      def esc(s)
-        Shellwords.escape(s)
       end
 
       def _process(template)
