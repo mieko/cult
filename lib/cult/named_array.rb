c3b24f2baa18be4665b54edea93f50e41da005b7
Mike Owens <mike@filespanker.com>
2016-08-16 01:36:43 -0400

    Elaborate on NamedArray for convenience

--- lib/cult/named_array.rb
+++ lib/cult/named_array.rb
@@ -1,3 +1,14 @@
+# A lot of times, we want a sequential array, of objects, but it'd still be
+# really convenient to refer to things by their name.  This is particularly
+# painful in the console, where, e.g., nodes can only be referred to by index,
+# and you end up calling `find` a lot.
+#
+# NamedArray is an array, but overloads [] to also work with a string or symbol.
+# e.g., nodes[:something].  It works by finding the first item who responds
+# from `named_array_identifier` with the matching key.
+#
+# By default named_array_identifier returns name, but this can be overridden.
+
 class Object
   def named_array_identifier
     name
@@ -5,6 +16,8 @@ class Object
 end
 
 module Cult
+
+  # Any Array can convert itself to a NamedArray
   module ArrayExtensions
     def to_named_array
       NamedArray.new(self)
@@ -12,11 +25,14 @@ module Cult
   end
   ::Array.prepend(ArrayExtensions)
 
+
   class NamedArray < Array
     def to_named_array
       self
     end
 
+    # Wrap any non-mutating methods that can return an Array,
+    # and wrap the result with a NamedArray
     superclass.instance_methods(false).each do |method_name|
       method_name = method_name.to_s
       unless ['?', '!'].include?(method_name[-1])
@@ -27,24 +43,36 @@ module Cult
       end
     end
 
-    def [](key)
-      case key
-        when Numeric
-          super
-        when String, Symbol
-          key = key.to_s
-          find { |v| v.named_array_identifier == key }
+    # Returns all keys that match if method == :select, the first if
+    # method == :find
+    def all(key, method = :select)
+      key = case key
+        when Integer
+          # Fallback to default behavior
+          return super
+        when String, Regexp, Proc, Range
+          key
+        when Symbol
+          key.to_s
         else
           fail ArgumentError
       end
+      send(method) { |v| key === v.named_array_identifier }
     end
 
-    def fetch(v)
-      self[v] or raise KeyError
+    # first matching item
+    def [](key)
+      return super if key.is_a?(Integer)
+      all(key, :find)
     end
 
-    def include?(v)
-      !! self[v]
+    # first matching item, or raises KeyError
+    def fetch(key)
+      all(key, :find) or raise KeyError
+    end
+
+    def key?(key)
+      !! all(key, :find)
     end
   end
 end
