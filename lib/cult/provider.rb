303bbc3c6ea362ff3bde755890dcac311d39c08c
Mike Owens <mike@filespanker.com>
2016-08-26 16:41:28 -0400

    Let providers have a defaults file

--- lib/cult/provider.rb
+++ lib/cult/provider.rb
@@ -46,12 +46,69 @@ module Cult
 
 
     def definition_path
-      File.join(path, "provider")
+      [File.join(path, "provider"), File.join(path, "defaults")]
     end
 
 
     def definition_parameters
       { project: self.project }
+    end
+
+    def definition_parents
+      []
+    end
+
+    # Chooses the smallest size setup with Ubuntu > Debian > Redhat,
+    # and a random zone.
+    def self.generate_defaults(definition)
+      definition = JSON.parse(definition.to_json)
+      text_to_mb = ->(text) {
+        multipliers = {
+          mb: 1 ** 1,
+          gb: 1 ** 2,
+          tb: 1 ** 3,
+          pb: 1 ** 4
+        }
+        if (m = text.match(/(\d+)([mgtp]b)/))
+          base = m[1].to_i
+          mul = multipliers.fetch(m[2].to_sym)
+          base * mul
+        else
+          nil
+        end
+      }
+
+      conf = definition['configurations']
+
+      # select the smallest size
+      size = conf['sizes'].map do |size|
+        if mb = text_to_mb.(size)
+          [mb, size]
+        else
+          nil
+        end
+      end.compact.sort_by(&:first).last.last
+
+      image = conf['images'].sort_by do |i|
+        case i
+          when /ubuntu-(\d+)-(\d+)/;
+            10000 + ($1.to_i * 100) + ($2.to_i * 10)
+          when /debian-(\d+)/
+            9000 + ($1.to_i * 100)
+          when /(redhat|centos|fedora)-(\d+)/
+            8000 + ($2.to_i * 100)
+          else
+            1
+        end
+      end.last
+
+      zone = conf['zones'].sample
+
+      {
+        default_size:  size,
+        default_zone:  zone,
+        default_image: image
+      }
     end
 
 
