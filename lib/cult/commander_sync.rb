683ed6921ab86ead8fb8d34d9c209d6b8eb119de
Mike Owens <mike@filespanker.com>
2016-09-15 07:24:01 -0400

    + CommanderSync

---
+++ lib/cult/commander_sync.rb
@@ -0,0 +1,30 @@
+module Cult
+  class CommanderSync
+    attr_reader :project, :nodes
+    def initialize(project:, nodes:)
+      @project, @nodes = project, nodes
+    end
+
+    def sync!(passes: nil)
+      if passes.nil? || passes.empty?
+        puts "calculating passes"
+        passes = required_passes
+      end
+
+      passes.each do |pass|
+        puts Rainbow("Executing pass #{pass}").yellow
+        nodes.each do |node|
+          c = Commander.new(project: project, node: node)
+          c.sync!(pass: pass)
+        end
+      end
+    end
+
+    def required_passes
+      # searches through every node and extracts which passes have to be ran
+      # to satisfy every event task
+      nodes.map(&:roles).flatten.uniq
+           .map(&:event_tasks).flatten.map(&:pass).uniq.sort
+    end
+  end
+end
