df4674d395df7f8e165b0b502e8f3e38a7f0a7e5
Mike A. Owens <mike@filespanker.com>
2016-09-19 06:49:57 -0400

    Paramap cleanup

--- lib/cult/paramap.rb
+++ lib/cult/paramap.rb
@@ -1,14 +1,22 @@
+# Cult.paramap runs a block in forked-off parallel processes.  There are very
+# little restrictions on what can be done in the block, but:
+#
+#  1. The value returned or any exceptions raised need to be Marshal-able.
+#  2. The blocks actually need to resume execution, so things like `exec`
+#     cause problems.
+#
+# The result of paramap is an array corresponding with each value ran through
+# the block.  There are a few exception strategies:
+#
+# 1. :raise The first job to throw an exception halts all further work and the
+#    exception is passed up
+# 2. :collect All jobs are allowed to complete, any exceptions encounted are
+#    tagged on the results in an `exception` method, which returns an array
+#    each element will either be 'nil' for no exception, or the Exception
+#    object the job raised.
+
 module Cult
   class Paramap
-    class MultipleExceptions < RuntimeError
-      attr_reader :exceptions
-
-      def initialize(exceptions)
-        super "mutiple exceptions raised"
-        @exceptions = exceptions
-      end
-    end
-
     class Job
       attr_reader :ident, :value, :block
       attr_reader :pid, :pipe
@@ -19,6 +27,7 @@ module Cult
         @pipe = IO.pipe
         @pid = fork do
           @pipe[0].close
+          prepare_forked_environment!
           begin
             write_response!(:result, block.call(value))
           rescue Exception => e
@@ -28,9 +37,29 @@ module Cult
         @pipe[1].close
       end
 
+      def prepare_forked_environment!
+        # Stub out things that have caused a problem in the past.
+        Kernel.define_method(:exec) do |*a|
+          fail "don't use Kernel\#exec inside of a paramap job"
+        end
+      end
+
+      def write_response!(status, obj)
+        begin
+          pipe[1].write(Marshal.dump([status, obj]))
+        rescue TypeError => e
+          # Unmarshallable
+          raise unless e.message.match(/_dump_data/)
+          pipe[1].write(Marshal.dump([status, nil]))
+        end
+        pipe[1].flush
+        pipe[1].close
+      end
+
       def fetch_response!
         unless pipe[0].closed?
-          obj = Marshal.load(@pipe[0].read)
+          data = @pipe[0].read
+          obj = Marshal.load(data)
           instance_variable_set("@#{obj[0]}", obj[1])
           pipe[0].close
         end
@@ -46,22 +75,10 @@ module Cult
         @exception
       end
 
-      def write_response!(status, obj)
-        begin
-          pipe[1].write(Marshal.dump([status, obj]))
-        rescue TypeError => e
-          # Unmarshallable
-          raise unless e.message.match(/_dump_data/)
-          pipe[1].write(Marshal.dump([status, nil]))
-        end
-        pipe[1].close
-      end
-
       def success?
         exception.nil?
       end
     end
-
 
     attr_reader :enum, :iter
     attr_reader :block
@@ -71,11 +88,11 @@ module Cult
     attr_reader :results
     attr_reader :concurrent
 
-    def initialize(enum, concurrent: nil, exception:, &block)
+    def initialize(enum, concurrent: nil, exception_strategy:, &block)
       @enum = enum
       @iter = @enum.to_enum
       @concurrent = concurrent || max_concurrent
-      @exception_strategy = exception
+      @exception_strategy = exception_strategy
       @block = block
       @exceptions, @results = [], []
       @job_queue = []
@@ -94,15 +111,10 @@ module Cult
       case exception_strategy
         when :raise
           raise job.exception
-        when :tag
-          results[job.ident] = nil
-          exceptions[job.ident] = job.exception
         when :collect
           exceptions.push(job.exception)
-        when :ignore
-          nil
-        when Proc
-          exception_strategy.call(job.exception)
+        else
+          fail "Bad exception_strategy: #{exception_strategy}"
       end
     end
 
@@ -134,12 +146,6 @@ module Cult
     end
 
     def report_exceptions(results)
-      if [:raise, :collect].include?(exception_strategy)
-        unless exceptions.empty?
-          raise MultipleExceptions.new(self.exceptions)
-        end
-      end
-
       self_exceptions = self.exceptions
       results.define_singleton_method(:exceptions) do
         self_exceptions
@@ -182,9 +188,11 @@ module Cult
       self.results
     end
   end
+  private_constant :Paramap
 
   module_function
-  def paramap(enum, exception: :raise, &block)
-    ::Cult::Paramap.new(enum, exception: exception, &block).run
+  def paramap(enum, concurrent: nil, exception: :raise, &block)
+    Paramap.new(enum, concurrent: concurrent,
+                exception_strategy: exception, &block).run
   end
 end
