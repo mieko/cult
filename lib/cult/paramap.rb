ce9d2d867a6cc4c7cebaf698ea4e94f82ca2c719
Mike A. Owens <mike@filespanker.com>
2016-09-19 05:06:29 -0400

    Refactor Paramap

--- lib/cult/paramap.rb
+++ lib/cult/paramap.rb
@@ -3,92 +3,85 @@ module Cult
     class MultipleExceptions < RuntimeError
       attr_reader :exceptions
 
-      def initialize(*exceptions)
+      def initialize(exceptions)
+        super "mutiple exceptions raised"
         @exceptions = exceptions
-        super("mutiple exceptions raised")
       end
     end
 
-    class Sub
-      attr_reader :pid
-      attr_reader :ident
+    class Job
+      attr_reader :ident, :value, :block
+      attr_reader :pid, :pipe
 
       def initialize(ident, value, block)
-        @ident = ident
+        @ident, @value, @block = ident, value, block
+
         @pipe = IO.pipe
         @pid = fork do
           @pipe[0].close
           begin
-            write_result!(:result, block.call(value))
+            write_response!(:result, block.call(value))
           rescue Exception => e
-            write_result!(:exception, e)
+            write_response!(:exception, e)
           end
         end
         @pipe[1].close
       end
 
-      def read_result!
-        if ! defined?(@finalized)
-          @finalized = true
+      def fetch_response!
+        unless pipe[0].closed?
           obj = Marshal.load(@pipe[0].read)
-          case obj[0]
-            when :result
-              @result = obj[1]
-            when :exception
-              @exception = obj[1]
-          end
-          @pipe.each(&:close)
+          instance_variable_set("@#{obj[0]}", obj[1])
+          pipe[0].close
         end
       end
 
       def result
-        read_result!
+        fetch_response!
         @result
       end
 
       def exception
-        read_result!
+        fetch_response!
         @exception
       end
 
-      def write_result!(status, obj)
+      def write_response!(status, obj)
         begin
-          @pipe[1].write(Marshal.dump([status, obj]))
+          pipe[1].write(Marshal.dump([status, obj]))
         rescue TypeError => e
-          if e.message.match(/_dump_data/)
-            @pipe[1].write(Marshal.dump([status, nil]))
-          else
-            raise
-          end
+          # Unmarshallable
+          raise unless e.message.match(/_dump_data/)
+          pipe[1].write(Marshal.dump([status, nil]))
         end
-        @pipe[1].flush
-        @pipe[1].close
+        pipe[1].close
       end
 
       def success?
-        read_result!
-        @exception.nil?
+        exception.nil?
       end
     end
 
 
-    attr_reader :enum
+    attr_reader :enum, :iter
     attr_reader :block
+    attr_reader :job_queue
     attr_reader :exception_strategy
     attr_reader :exceptions
     attr_reader :results
+    attr_reader :concurrent
 
-
-    def initialize(enum, exception:, &block)
+    def initialize(enum, concurrent: nil, exception:, &block)
       @enum = enum
+      @iter = @enum.to_enum
+      @concurrent = concurrent || max_concurrent
       @exception_strategy = exception
       @block = block
-      @exceptions = []
-      @results = []
+      @exceptions, @results = [], []
+      @job_queue = []
     end
 
-
-    def max_parallel
+    def max_concurrent
       case (r = Cult.concurrency)
         when :max
           enum.respond_to?(:size) ? enum.size : 200
@@ -97,69 +90,95 @@ module Cult
       end
     end
 
-    def handle_exception(sub)
+    def handle_exception(job)
       case exception_strategy
         when :raise
-          raise sub.exception
+          raise job.exception
         when :tag
-          results[sub.ident] = nil
-          exceptions[sub.ident] = sub.exception
+          results[job.ident] = nil
+          exceptions[job.ident] = job.exception
         when :collect
-          exceptions.push(sub.exception)
+          exceptions.push(job.exception)
         when :ignore
           nil
         when Proc
-          exception_strategy.call(sub.exception)
+          exception_strategy.call(job.exception)
       end
     end
 
-    def handle_result(sub)
-      results[sub.ident] = sub.result
+    def handle_result(job)
+      results[job.ident] = job.result
     end
 
-    def handle_response(sub)
-      sub.success? ? handle_result(sub) : handle_exception(sub)
+    def handle_response(job)
+      job.success? ? handle_result(job) : handle_exception(job)
     end
 
-    def run(njobs = max_parallel)
-      iter = enum.to_enum
-      active = []
-      finished = false
-      i = 0
-
-      loop do
-        while !finished && active.size != njobs
-          begin
-            next_value = iter.next
-          rescue StopIteration
-            finished = true
-            break
-          end
-          active.push(Sub.new(i, next_value, block))
-          i += 1
-        end
-
-        if active.empty?
-          break if finished
-        else
-          pid = Process.waitpid
-          if (sub = active.find {|sub| sub.pid == pid})
-            active.delete(sub)
-            handle_response(sub)
-          end
-        end
+    def new_job_index
+      (@job_index ||= 0).tap do
+        @job_index += 1
       end
+    end
 
+    def add_job(value)
+      job_queue.push(Job.new(new_job_index, value, block))
+    end
+
+    def job_by_pid(pid)
+      job_queue.find { |job| job.pid == pid }
+    end
+
+    def process_finished_job(job)
+      job_queue.delete(job)
+      handle_response(job)
+    end
+
+    def report_exceptions(results)
       if [:raise, :collect].include?(exception_strategy)
-        unless self.exceptions.empty?
-          raise MultipleExceptions.new(*self.exceptions)
+        unless exceptions.empty?
+          raise MultipleExceptions.new(self.exceptions)
         end
       end
 
-      e = self.exceptions
-      self.results.define_singleton_method(:exceptions) do
-        e
+      self_exceptions = self.exceptions
+      results.define_singleton_method(:exceptions) do
+        self_exceptions
       end
+    end
+
+    def job_queue_full?
+      job_queue.size == concurrent
+    end
+
+    def more_tasks?
+      iter.peek
+      true
+    rescue StopIteration
+      false
+    end
+
+    def next_task
+      iter.next
+    end
+
+    def queue_next_task
+      add_job(next_task)
+    end
+
+    def wait_for_next_job_to_finish
+      if (job = job_by_pid(Process.waitpid))
+        process_finished_job(job)
+      end
+    end
+
+    def run
+      loop do
+        queue_next_task until job_queue_full? || !more_tasks?
+        break if job_queue.empty? && ! more_tasks?
+        wait_for_next_job_to_finish
+      end
+
+      report_exceptions(self.results)
       self.results
     end
   end
